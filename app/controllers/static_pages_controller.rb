76e7419cb1d2ff9d9797e9aae0a27d13159d4e74
Vidush Goswami <goswamiv@grinnell.edu>
2020-02-20 01:04:52 -0600

    Added Random Hall Generator

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,10 @@ class StaticPagesController < ApplicationController
   def about
       end
   
+  def hall
+    val = rand(1...8)
+    arr = "Younker","Main","Smith","Haines","Dibble","Lazier","Clangrala","Loose"
+    @result = arr[val]
+  end
+  
 end
