9f66113dfcba5d5d5b495f570da54ee98528bab0
Vidush Goswami <goswamiv@grinnell.edu>
2020-04-01 22:52:50 -0500

    Database trial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
- 
+
+  get 'microposts/index'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/hall' => 'static_pages#hall'
-  
+  resources :microposts
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Albert",email: "albert@grinnell.edu")
+User.create(name: "John",email: "john@grinnell.edu")
+User.create(name: "Bob",email: "bob@grinnell.edu")
+User.create(name: "Jack",email: "jack@grinnell.edu")
+
+Micropost.create(content: "Younker 9113", user_id: User.find(1).email)
+Micropost.create(content: "Dibble 2344", user_id: User.find(2).email)
+Micropost.create(content: "Rose 4565", user_id: User.find(3).email)
+Micropost.create(content: "Loose 3345", user_id: User.find(4).email)
